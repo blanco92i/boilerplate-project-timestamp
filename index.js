31d82fe665624bdf1bad1052a4659394d0523f8c
blanco92i <blancotradizi@gmail.com>
2024-11-07 22:08:15 +0000

    update

--- index.js
+++ index.js
@@ -13,11 +13,40 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use((req,res,next)=>{
+  console.log(
+    `${req.method} ${req.path} - ${req.ip}`
+  )
+  next()
+})
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?" ,(req,res)=>{
+  let dateParm = req.params.date;
+  if(!dateParm){
+    date = new Date();
+  }else{
+    if(!isNaN(dateParm)){
+      dateParm = parseInt(dateParm)
+    }
+    date = new Date(dateParm);
+  }
+
+
+  // Check if the date is valid
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({
+      unix: date.getTime(), // Unix timestamp in milliseconds
+      utc: date.toUTCString()
+    });
+  }
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
